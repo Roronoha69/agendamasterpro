0a8d470e432e8764706e7886b45cd7195a510fbf
Justin M'BODJE <justin.mbodje@niji.fr>
2024-04-30 10:45:18 +0200

    Added Angular routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/agenda', pathMatch: 'full' },
+  { path: 'agenda', component: AgendaComponent },
+  { path: 'profile', component: UserComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
