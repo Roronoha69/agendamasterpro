4ff79e4aa0749fe917dc4ec0a72e723e997aee86
Justin M'BODJE <justin.mbodje@niji.fr>
2024-04-30 11:45:53 +0200

    Add services & Airtable.

--- src/app/Components/agenda/agenda.component.ts
+++ src/app/Components/agenda/agenda.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/Services/api.service';
 
 @Component({
   selector: 'app-agenda',
   templateUrl: './agenda.component.html',
   styleUrls: ['./agenda.component.scss']
 })
-export class AgendaComponent {
 
+
+export class AgendaComponent implements OnInit{
+  
+  constructor(
+    public apiService: ApiService
+  ){}
+
+  ngOnInit(): void {
+      this.apiService.getUsers()
+  }
 }
--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -6,4 +6,26 @@ import { Injectable } from '@angular/core';
 export class ApiService {
 
   constructor() { }
+
+  // fetchDataFromAirtable() {
+  //   const apiKey = 'patoaPnmA3wb6mjBZ.c7e3f2e25ea7b9f17cd410c08634ce5de6281550c3be4acb7970ba87d486f704';
+  //   const tableId = 'YOUR_TABLE_ID';
+  //   const apiUrl = `https://api.airtable.com/v0/${tableId}/YourTable`;
+
+  //   const headers = {
+  //     'Authorization': `Bearer ${apiKey}`
+  //   };
+
+  //   return axios.get(apiUrl, { headers })
+  //     .then(response => response.data)
+  //     .catch(error => {
+  //       console.error('Error fetching data from Airtable:', error);
+  //       throw error;
+  //     });
+  // }
+
+  public getUsers(){
+    console.log("hello");
+    
+  }
 }
